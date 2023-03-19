ff82ca6f22e6b48158ef1497dfb677e03618813f
ricky-kiva <ricky.kiva@gmail.com>
2023-03-19 15:22:33 +0700

    add Search Menu Action

--- app/src/main/java/com/rickyslash/actionbarapp/MainActivity.kt
+++ app/src/main/java/com/rickyslash/actionbarapp/MainActivity.kt
@@ -1,10 +1,14 @@
 package com.rickyslash.actionbarapp
 
+import android.app.SearchManager
+import android.content.Context
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.Menu
 import android.view.MenuItem
+import android.widget.Toast
+import androidx.appcompat.widget.SearchView
 import com.rickyslash.actionbarapp.databinding.ActivityMainBinding
 
 private lateinit var binding: ActivityMainBinding
@@ -21,6 +25,35 @@ class MainActivity : AppCompatActivity() {
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
         val inflater = menuInflater
         inflater.inflate(R.menu.option_menu, menu)
+
+        // 'getSystemService' returns the 'service object' (SEARCH SERVICE) which need to be typecasted to 'SearchManager'
+        val searchManager = getSystemService(Context.SEARCH_SERVICE) as SearchManager
+
+        // this will get the 'search' from 'menu XML' file
+        // '.actionView' passed because this view got 'actionViewClass' property in the 'XML'
+        // 'as SearchView' will typecast 'actionView' as 'SearchView'
+        val searchView = menu?.findItem(R.id.search)?.actionView  as SearchView
+
+        // '.getSearchableInfo' will get 'configuration for search interface'
+        // '.setSearchableInfo' will set the 'object' (from getSearchableInfo()) to 'searchView'
+        searchView.setSearchableInfo(searchManager.getSearchableInfo(componentName))
+        // after the 'search interface configuration' 'has been set', we could set 'queryHint'
+        // 'queryHint' is the 'text displayed' in 'collapsed search view'
+        searchView.queryHint = resources.getString(R.string.search_hint)
+        // 'setOnQueryTextListener' will set the 'function' when 'query' 'passed'
+        searchView.setOnQueryTextListener(object : SearchView.OnQueryTextListener {
+            // this 'function run' when user 'submitted' the 'query'
+            override fun onQueryTextSubmit(query: String?): Boolean {
+                Toast.makeText(this@MainActivity, query, Toast.LENGTH_SHORT).show()
+                searchView.clearFocus()
+                return true
+            }
+
+            // this tells it will 'react nothing' when the 'text change'
+            override fun onQueryTextChange(newText: String?): Boolean {
+                return false
+            }
+        })
         return true
     }
 
