2cd817ed670c028b493e3a0bcfdcb6bd36f82f8a
ricky-kiva <ricky.kiva@gmail.com>
2023-03-19 10:52:30 +0700

    add menu actions

--- app/src/main/java/com/rickyslash/actionbarapp/MainActivity.kt
+++ app/src/main/java/com/rickyslash/actionbarapp/MainActivity.kt
@@ -1,11 +1,45 @@
 package com.rickyslash.actionbarapp
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import com.rickyslash.actionbarapp.databinding.ActivityMainBinding
+
+private lateinit var binding: ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        // make binding
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+    }
+
+    // make menu displayed on this Activity
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        val inflater = menuInflater
+        inflater.inflate(R.menu.option_menu, menu)
+        return true
+    }
+
+    // organize menu items action when clicked
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            R.id.menu1 -> {
+                supportFragmentManager.beginTransaction()
+                    .replace(R.id.fragment_container, MenuFragment())
+                    .addToBackStack(null)
+                    .commit()
+                return true
+            }
+            R.id.menu2 -> {
+                val i = Intent(this, MenuActivity::class.java)
+                startActivity(i)
+                return true
+            }
+            else -> return true
+        }
     }
 }
